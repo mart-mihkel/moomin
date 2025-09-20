cb465003ffb4f747c8b27dfdb5897994f85d5335
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-09-20 18:38:16 +0300

    dunst

--- scripts/hellwal-post.sh
+++ scripts/hellwal-post.sh
@@ -3,8 +3,10 @@
 CACHE=$HOME/.cache/hellwal
 CONFIG=$HOME/.config
 
+mkdir -p $CONFIG/dunst
+
 cp $CACHE/waybar.css $CONFIG/waybar/style.css
-cp $CACHE/dunstrc $CONFIG/dunst/dunstrc
+cp $CACHE/dunst.toml $CONFIG/dunst/dunstrc
 
 pkill -SIGUSR2 waybar
 dunstctl reload
