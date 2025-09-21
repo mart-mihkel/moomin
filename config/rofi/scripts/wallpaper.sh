64bb7ec9cbf32efb71c971c2ec59043c99036eb1
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-09-21 10:04:12 +0300

    rofi wallpaper mode icons

--- config/rofi/scripts/wallpaper.sh
+++ config/rofi/scripts/wallpaper.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
 WALLS=~/Pictures/walls
+CACHE=$HOME/.cache/hellwal
+CONFIG=$HOME/.config
 
 if [ ! -z $1 ]; then
+    WALLPAPER=$WALLS/$1
     coproc (
-        hellwal -l -s ~/.local/bin/hellwal-post.sh -i $WALLS/$1
-        swww img $WALLS/$1 -t fade
+        hellwal -l -i $WALLPAPER
+        swww img $WALLPAPER -t fade
+
+        mkdir -p $CONFIG/dunst
+        cp $CACHE/dunst.toml $CONFIG/dunst/dunstrc
+
+        pkill -SIGUSR2 waybar
+        dunstctl reload
     )
 
     exit 0
 fi
 
-ls $WALLS
+for WALLPAPER in $WALLS/*; do
+    BN=$(basename $WALLPAPER)
+    echo -e "${BN}\0icon\x1f${WALLPAPER}"
+done
